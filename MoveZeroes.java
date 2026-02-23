f46b0d09c98c7bfbcc15512e50c44370f7db29d0
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-23 20:40:37 +0530

    Move Zeroes

---
+++ MoveZeroes.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public static int[] moveZeroes(int nums[]){
+        int pos = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        while(pos < nums.length){
+            nums[pos] = 0;
+            pos++;
+        }
+        return nums;
+    }
+    public static void main(String args[]){
+        int nums[] = {0,1,0,3,12};
+        System.out.println(Arrays.toString(moveZeroes(nums)));
+    }
+}
