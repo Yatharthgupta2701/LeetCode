5ad1e6c7c2b5c00fd2aec26d5a2db5143000e7d0
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-02-26 23:37:38 +0530

    Majority Element

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+public class MajorityElement {
+
+    public static int majorityElement(int nums[]){
+        int candidate = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(count == 0) {
+                candidate = nums[i];
+            }
+            if(nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+    public static void main(String args[]){
+        int nums[] = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+}
