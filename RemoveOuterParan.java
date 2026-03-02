3444a537f05ebd29ce44dd0641ac9d74082af226
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-03-02 23:00:19 +0530

    Remove Outermost Parenthese

---
+++ RemoveOuterParan.java
@@ -0,0 +1,26 @@
+public class RemoveOuterParan {
+
+    public static String removeOuterParan(String s){
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (count > 0) {
+                    result.append(ch);
+                }
+                count++;
+            } 
+            else {
+                count--;
+                if (count > 0) {
+                    result.append(ch);
+                }
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String args[]){
+        String s = "(()())(())";
+        System.out.println(removeOuterParan(s));
+    }
+}
