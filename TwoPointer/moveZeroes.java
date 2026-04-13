d7a8bc1e58b4bff93b9c8ffda217c06f11fcc4ab
Yatharthgupta2701 <gyatharth086@gmail.com>
2026-04-13 08:01:01 +0530

    Move Zeroes Problem Using Two  Pointer

---
+++ TwoPointer/moveZeroes.java
@@ -0,0 +1,22 @@
+package TwoPointer;
+public class moveZeroes {
+
+    public static void MoveZeroes(int nums[]){
+        int left = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }
+    }
+    public static void main(String args[]){
+        int nums[] = {0,1,0,3,12};
+        MoveZeroes(nums);
+        for(int num : nums){
+            System.out.print(num +" ");
+        }
+    }
+}
